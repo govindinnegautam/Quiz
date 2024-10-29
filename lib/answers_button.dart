1fff0449f433124ac3d84f598628dbbb97e82337
govindinnegautam <156452884+govindinnegautam@users.noreply.github.com>
2024-10-29 18:51:21 +0530

    Add files via upload

---
+++ lib/answers_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({
+    super.key,
+    required this.answerText,
+    required this.onTap,
+  });
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 40,
+          ),
+          backgroundColor: const Color.fromARGB(255, 77, 79, 79),
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
+          ),
+        ),
+        child: SizedBox(
+          width: 200,
+          child: Text(
+            answerText,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
